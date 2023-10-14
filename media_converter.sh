e26556fc864c0bb46f88629bcced5bfec02b2dd5
desktop <haydenguyn@gmail.com>
2023-10-13 18:24:41 -0700

    Added logic to check what conversion type the user is doing (video, audio, image)

--- media_converter.sh
+++ media_converter.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $(basename "$0") <file to convert> <format to convert to>"
+if [ $# -ne 1 ]; then
+    echo "Usage: $(basename "$0") <file/dir to convert>"
     exit 1
 fi
 
-echo "$1 $2"
+echo -e "\n"
+PS3="What would you like to convert your file(s) to? "
+options=("Video" "Audio" "Image" "Exit")
+
+conversion_type=""
+
+select choice in "${options[@]}"; do
+    case "$choice" in
+        "Video")
+            conversion_type="video"
+            break
+            ;;
+        "Audio")
+            conversion_type="audio"
+            break
+            ;;
+        "Image")
+            conversion_type="image"
+            break
+            ;;
+        "Exit")
+            exit 1
+            ;;
+        *)
+            echo -e "\nInvalid option. Please enter a number from 1 to ${#options[@]}\n"
+            ;;
+    esac
+done
+
+echo $conversion_type
